9a301eddfe239fa232925ceb1104cae35802a9f1
edy <118206564+edyarto@users.noreply.github.com>
2022-11-15 10:11:47 +0800

    Create Test1.java

---
+++ src/test/poc/Test1.java
@@ -0,0 +1,14 @@
+package test.poc;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
+		System.out.println("line5");
+	}
+
+}
